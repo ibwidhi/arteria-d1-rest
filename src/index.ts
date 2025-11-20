0805a73d9e59c21248a22c86b2b63da1b2270157
Widhi Adnyana <tudewidhi@gmail.com>
2025-11-21 02:42:26 +0800

    chore: setup hono rest api

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,33 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+// export default {
+// 	async fetch(request, env, ctx): Promise<Response> {
+// 		return new Response('Hello World!');
+// 	},
+// } satisfies ExportedHandler<Env>;
+
+import { Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+type Env = {
+	DB: D1Database;
+};
+
+const app = new Hono<{ Bindings: Env }>();
+app.use('/api/*', cors());
+
+app.get('/api/webhooks', async (c) => {
+	const { results } = await c.env.DB.prepare('SELECT * FROM webhooks ORDER BY created_at DESC').all();
+	return c.json(results);
+});
+
+app.post('/api/webhooks', async (c) => {
+	const body = await c.req.json();
+	const stringPayload = JSON.stringify(body);
+	console.log('body ::: ', stringPayload);
+	await c.env.DB.prepare('INSERT INTO webhooks (payload) VALUES (?)').bind(stringPayload).run();
+	return c.json({ success: true });
+});
+
+export default app;
